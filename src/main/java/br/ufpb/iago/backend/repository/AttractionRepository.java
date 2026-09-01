81ce344fd57247bf3a801743253b77c660e0eac7
iago <iago.alves@dcx.ufpb.br>
2026-09-01 18:20:20 -0300

    feat: adicao da busca usando PostGis no Repository

--- src/main/java/br/ufpb/iago/backend/repository/AttractionRepository.java
+++ src/main/java/br/ufpb/iago/backend/repository/AttractionRepository.java
@@ -2,8 +2,25 @@ package br.ufpb.iago.backend.repository;
 
 import br.ufpb.iago.backend.model.Attraction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AttractionRepository extends JpaRepository<Attraction, UUID> {
+
+    @Query(value = """
+            SELECT * FROM attractions a 
+            WHERE ST_DWithin(
+                CAST(a.location AS geography), 
+                CAST(ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326) AS geography), 
+                :radiusInMeters
+            )
+            """, nativeQuery = true)
+    List<Attraction> findAttractionsWithinRadius(
+            @Param("latitude") double latitude,
+            @Param("longitude") double longitude,
+            @Param("radiusInMeters") double radiusInMeters
+    );
 }
